c4cd5e3911c1887ba1fe74fdfb68c7edf317fd32
Muhammad M. Usman <mmusmanlab@gmail.com>
2026-05-01 20:53:44 +0100

    Fix formatting in sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,28 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://mmusmanlab.com";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+  ];
+}

40b00fed413a6b7f7672037b8f255459e4e465a0
Muhammad M. Usman <mmusmanlab@gmail.com>
2026-05-01 22:09:39 +0100

    Add Google site verification meta tag
    Added Google site verification meta tag for SEO.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <meta name="google-site-verification" content="esWBVqFGk0x2cILK-CW0C8qQ2pk0w8gi92y4KxIImVc" />
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
